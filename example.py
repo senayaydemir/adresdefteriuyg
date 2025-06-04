052fa1bf286638e50e6ce3de0d945800ddde033b
senayaydemir <senayaydemir00@gmail.com>
2025-06-05 01:59:55 +0300

    Proje tamamlandı

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ def secenekler():
     print("3-Kişileri Göster")
     print("4-Çıkış")
 
-    def kisi_ekle():
+def kisi_ekle():
         isim=input("İsim:")
         telefon=input("Telefon:")
         email=input("Email:")
@@ -15,19 +15,32 @@ def secenekler():
 
 
 
-    def kisi_sil():
+def kisi_sil():
         kisi_no=int(input("Silmek istediğiniz kişinin numarasını giriniz:"))
         if 0<=kisi_no <len(adres_defteri):
          silinen_kisi=adres_defteri.pop(kisi_no)
          print(f"{silinen_kisi['isim']}Adres defterindeen silindi")
 
-    def kisileri_goster():
+def kisileri_goster():
        if not adres_defteri: 
           print("Adres defteri boş")
-        else:
+       else:
           print("\nAdres Defteri")
           for i,kisi in enumerate(adres_defteri,1):
              print(f"{i}. İsim:{kisi['isim']},Telefon:{kisi['Telefon']},Email:{kisi['isim']}")
 while True:
-   secenekler()
-   secim=int(input("Lütfen yapmak istediğiniz işlemi seçiniz:"))
+    secenekler()
+    secim=int(input("Lütfen yapmak istediğiniz işlemi seçiniz:"))
+
+    if secim == 1:
+      kisi_ekle()
+    elif secim ==2:
+      kisi_sil()
+    elif secim ==3:
+      kisileri_goster()
+    elif secim ==4:
+      print("Programdan Çıkılıyor...")
+      break
+    
+    else:
+      print("Geçersiz seçim.Lütfen tekrar deneyiniz...")      
